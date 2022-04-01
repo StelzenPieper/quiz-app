9b860d3db60fe42354cea14f839710a2e5d43f56
Sebastian Ottow <seb.ottow@gmail.com>
2022-04-01 11:58:56 +0200

    Add utility.css sheet and classes to toggle show/hide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
 //query Selector BOOKMARK
 const bookmark = document.querySelector('[data-js="bookmark"]');
 
+//query Selector SHOW-ANSWER Button
+const showAnswerBtn = document.querySelector('[data-js="showAnswerBtn"]');
+
+//query Selector SHOW-ANSWER
+const showAnswer = document.querySelector(".card__answer");
+
+//query Selector HIDE-ANSWER Button
+const hideAnswerBtn = document.querySelector('[data-js="hideAnswerBtn"]');
+
+
 //EventListener BOOKMARK-CLICK
 bookmark.addEventListener('click', () => {
     bookmark.classList.toggle("bookmarked");
 })
 
-//query Selector SHOW-ANSWER Button
-const showAnswerBtn = document.querySelector('[data-js="showAnswerBtn"]');
-const showAnswer = document.querySelector(".card__answer");
-//query Selector HIDE-ANSWER Button
-const hideAnswerBtn = document.querySelector('[data-js="hideAnswerBtn"]');
-
 //EventListener SHOW-ANSWER Button
 showAnswerBtn.addEventListener('click', () => {
     showAnswer.classList.toggle("card__showanswer");
-    showAnswerBtn.classList.toggle("btn__answer--show");
+    showAnswerBtn.classList.toggle("btn__answer--hide");
     hideAnswerBtn.classList.toggle("btn__answer--show");
 })
 
@@ -23,6 +27,6 @@ showAnswerBtn.addEventListener('click', () => {
 //EventListener HIDE-ANSWER Button
 hideAnswerBtn.addEventListener('click', () => {
     showAnswer.classList.toggle("card__showanswer");
-    showAnswerBtn.classList.toggle("btn__answer--show");
+    showAnswerBtn.classList.toggle("btn__answer--hide");
     hideAnswerBtn.classList.toggle("btn__answer--show");
 })
