5b457e49bc320bf52dd88e91d094019677527eba
Sebastian Ottow <seb.ottow@gmail.com>
2022-04-07 15:40:34 +0200

    Add more extensions for linting

--- assets/components/nav/nav.js
+++ assets/components/nav/nav.js
@@ -1,63 +1,61 @@
-export default function Navigation () {
+export default function Navigation() {
+  const homeBtn = document.querySelector('[data-js="home"]');
+  const homeContent = document.querySelector('[data-js="home-site"]');
 
-    const homeBtn = document.querySelector('[data-js="home"]');
-    const homeContent = document.querySelector('[data-js="home-site"]');
+  const bookmarkBtn = document.querySelector('[data-js="bookmarks"]');
+  const bookmarkContent = document.querySelector('[data-js="bookmarks-site"]');
 
-    const bookmarkBtn = document.querySelector('[data-js="bookmarks"]');
-    const bookmarkContent = document.querySelector('[data-js="bookmarks-site"]');
+  const createBtn = document.querySelector('[data-js="create"]');
+  const createContent = document.querySelector('[data-js="create-site"]');
 
-    const createBtn = document.querySelector('[data-js="create"]');
-    const createContent = document.querySelector('[data-js="create-site"]');
+  const profileBtn = document.querySelector('[data-js="profile"]');
+  const profileContent = document.querySelector('[data-js="profile-site"]');
 
-    const profileBtn = document.querySelector('[data-js="profile"]');
-    const profileContent = document.querySelector('[data-js="profile-site"]');
+  homeBtn.addEventListener("click", () => {
+    bookmarkContent.classList.add("hide");
+    homeContent.classList.remove("hide");
+    createContent.classList.add("hide");
+    profileContent.classList.add("hide");
 
-    homeBtn.addEventListener('click', () => {
-        bookmarkContent.classList.add("hide");
-        homeContent.classList.remove("hide");
-        createContent.classList.add("hide");
-        profileContent.classList.add("hide");
+    homeBtn.classList.add("nav__item--clicked");
+    bookmarkBtn.classList.remove("nav__item--clicked");
+    createBtn.classList.remove("nav__item--clicked");
+    profileBtn.classList.remove("nav__item--clicked");
+  });
 
-        homeBtn.classList.add("nav__item--clicked")
-        bookmarkBtn.classList.remove("nav__item--clicked")
-        createBtn.classList.remove("nav__item--clicked")
-        profileBtn.classList.remove("nav__item--clicked")
-    });
+  bookmarkBtn.addEventListener("click", () => {
+    bookmarkContent.classList.remove("hide");
+    homeContent.classList.add("hide");
+    createContent.classList.add("hide");
+    profileContent.classList.add("hide");
 
-    bookmarkBtn.addEventListener('click', () => {
-        bookmarkContent.classList.remove("hide");
-        homeContent.classList.add("hide");
-        createContent.classList.add("hide");
-        profileContent.classList.add("hide");
+    homeBtn.classList.remove("nav__item--clicked");
+    bookmarkBtn.classList.add("nav__item--clicked");
+    createBtn.classList.remove("nav__item--clicked");
+    profileBtn.classList.remove("nav__item--clicked");
+  });
 
-        homeBtn.classList.remove("nav__item--clicked")
-        bookmarkBtn.classList.add("nav__item--clicked")
-        createBtn.classList.remove("nav__item--clicked")
-        profileBtn.classList.remove("nav__item--clicked")
-    });
+  createBtn.addEventListener("click", () => {
+    bookmarkContent.classList.add("hide");
+    homeContent.classList.add("hide");
+    createContent.classList.remove("hide");
+    profileContent.classList.add("hide");
 
-    createBtn.addEventListener('click', () => {
-        bookmarkContent.classList.add("hide");
-        homeContent.classList.add("hide");
-        createContent.classList.remove("hide");
-        profileContent.classList.add("hide");
+    homeBtn.classList.remove("nav__item--clicked");
+    bookmarkBtn.classList.remove("nav__item--clicked");
+    createBtn.classList.add("nav__item--clicked");
+    profileBtn.classList.remove("nav__item--clicked");
+  });
 
-        homeBtn.classList.remove("nav__item--clicked")
-        bookmarkBtn.classList.remove("nav__item--clicked")
-        createBtn.classList.add("nav__item--clicked")
-        profileBtn.classList.remove("nav__item--clicked")
-    });
+  profileBtn.addEventListener("click", () => {
+    bookmarkContent.classList.add("hide");
+    homeContent.classList.add("hide");
+    createContent.classList.add("hide");
+    profileContent.classList.remove("hide");
 
-    profileBtn.addEventListener('click', () => {
-        bookmarkContent.classList.add("hide");
-        homeContent.classList.add("hide");
-        createContent.classList.add("hide");
-        profileContent.classList.remove("hide");
-
-        homeBtn.classList.remove("nav__item--clicked")
-        bookmarkBtn.classList.remove("nav__item--clicked")
-        createBtn.classList.remove("nav__item--clicked")
-        profileBtn.classList.add("nav__item--clicked")
-    });
-
+    homeBtn.classList.remove("nav__item--clicked");
+    bookmarkBtn.classList.remove("nav__item--clicked");
+    createBtn.classList.remove("nav__item--clicked");
+    profileBtn.classList.add("nav__item--clicked");
+  });
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,11 @@ import Quizcard from "../components/quizcards/quizcards.js";
 const cardElements = document.querySelectorAll('[data-js="quiz-card"]');
 
 cardElements.forEach((cardElement) => {
-    Quizcard(cardElement);
+  Quizcard(cardElement);
 });
 
 //import Navigation from "../components/nav/nav.js";
 
-
 import Navigation from "../components/nav/nav.js";
 
 Navigation();
-
-
-
-
-
